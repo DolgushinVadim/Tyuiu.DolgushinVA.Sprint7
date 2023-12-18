82839ee3186ebd84175548686cadce1c89ef9256
Tyuiu.DolgushinVA@outlook.com <vadimdolgushin9@gmail.com>
2023-12-18 16:02:28 +0500

    V1.1

--- Tyuiu.DolgushinVA.Sprint7.Project.V12.Test/DataServiceTest.cs
+++ Tyuiu.DolgushinVA.Sprint7.Project.V12.Test/DataServiceTest.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
+using System.IO;
+using Tyuiu.DolgushinVA.Sprint7.Project.V12.Lib;
 namespace Tyuiu.DolgushinVA.Sprint7.Project.V12.Test
 {
     [TestClass]
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void LoadFromDataFile()
         {
+            string path = @"C:\DataSprint7\БД Персональные ЭВМ.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            Assert.AreEqual(true, fileExists);
         }
     }
 }
