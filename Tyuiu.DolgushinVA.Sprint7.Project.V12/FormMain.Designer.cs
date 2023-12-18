710022b62d2f38dfc1589e483b0f7267d4390938
Tyuiu.DolgushinVA@outlook.com <vadimdolgushin9@gmail.com>
2023-12-18 23:20:01 +0500

    V1.1

---
+++ Tyuiu.DolgushinVA.Sprint7.Project.V12/FormMain.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace Tyuiu.DolgushinVA.Sprint7.Project.V12
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelLeft_DVA = new System.Windows.Forms.Panel();
+            this.panelRight_DVA = new System.Windows.Forms.Panel();
+            this.panelDown_DVA = new System.Windows.Forms.Panel();
+            this.panelMain_DVA = new System.Windows.Forms.Panel();
+            this.groupBoxStatistic_DVA = new System.Windows.Forms.GroupBox();
+            this.dataGridViewViewFile_DVA = new System.Windows.Forms.DataGridView();
+            this.groupBoxFilter_DVA = new System.Windows.Forms.GroupBox();
+            this.groupBoxSort_DVA = new System.Windows.Forms.GroupBox();
+            this.textBoxSearch_DVA = new System.Windows.Forms.TextBox();
+            this.labelSearch_DVA = new System.Windows.Forms.Label();
+            this.panelMain_DVA.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewViewFile_DVA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelLeft_DVA
+            // 
+            this.panelLeft_DVA.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelLeft_DVA.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft_DVA.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft_DVA.Name = "panelLeft_DVA";
+            this.panelLeft_DVA.Size = new System.Drawing.Size(25, 646);
+            this.panelLeft_DVA.TabIndex = 0;
+            // 
+            // panelRight_DVA
+            // 
+            this.panelRight_DVA.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelRight_DVA.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRight_DVA.Location = new System.Drawing.Point(1357, 0);
+            this.panelRight_DVA.Name = "panelRight_DVA";
+            this.panelRight_DVA.Size = new System.Drawing.Size(25, 646);
+            this.panelRight_DVA.TabIndex = 1;
+            // 
+            // panelDown_DVA
+            // 
+            this.panelDown_DVA.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelDown_DVA.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelDown_DVA.Location = new System.Drawing.Point(25, 621);
+            this.panelDown_DVA.Name = "panelDown_DVA";
+            this.panelDown_DVA.Size = new System.Drawing.Size(1332, 25);
+            this.panelDown_DVA.TabIndex = 2;
+            // 
+            // panelMain_DVA
+            // 
+            this.panelMain_DVA.Controls.Add(this.groupBoxSort_DVA);
+            this.panelMain_DVA.Controls.Add(this.labelSearch_DVA);
+            this.panelMain_DVA.Controls.Add(this.textBoxSearch_DVA);
+            this.panelMain_DVA.Controls.Add(this.groupBoxFilter_DVA);
+            this.panelMain_DVA.Controls.Add(this.dataGridViewViewFile_DVA);
+            this.panelMain_DVA.Controls.Add(this.groupBoxStatistic_DVA);
+            this.panelMain_DVA.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain_DVA.Location = new System.Drawing.Point(25, 0);
+            this.panelMain_DVA.Name = "panelMain_DVA";
+            this.panelMain_DVA.Size = new System.Drawing.Size(1332, 621);
+            this.panelMain_DVA.TabIndex = 3;
+            // 
+            // groupBoxStatistic_DVA
+            // 
+            this.groupBoxStatistic_DVA.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBoxStatistic_DVA.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.groupBoxStatistic_DVA.Location = new System.Drawing.Point(6, 509);
+            this.groupBoxStatistic_DVA.Name = "groupBoxStatistic_DVA";
+            this.groupBoxStatistic_DVA.Size = new System.Drawing.Size(1043, 106);
+            this.groupBoxStatistic_DVA.TabIndex = 0;
+            this.groupBoxStatistic_DVA.TabStop = false;
+            this.groupBoxStatistic_DVA.Text = "Статистика";
+            // 
+            // dataGridViewViewFile_DVA
+            // 
+            this.dataGridViewViewFile_DVA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewViewFile_DVA.Location = new System.Drawing.Point(6, 57);
+            this.dataGridViewViewFile_DVA.Name = "dataGridViewViewFile_DVA";
+            this.dataGridViewViewFile_DVA.RowHeadersWidth = 51;
+            this.dataGridViewViewFile_DVA.RowTemplate.Height = 24;
+            this.dataGridViewViewFile_DVA.Size = new System.Drawing.Size(1043, 446);
+            this.dataGridViewViewFile_DVA.TabIndex = 1;
+            // 
+            // groupBoxFilter_DVA
+            // 
+            this.groupBoxFilter_DVA.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.groupBoxFilter_DVA.Location = new System.Drawing.Point(1055, 57);
+            this.groupBoxFilter_DVA.Name = "groupBoxFilter_DVA";
+            this.groupBoxFilter_DVA.Size = new System.Drawing.Size(271, 105);
+            this.groupBoxFilter_DVA.TabIndex = 2;
+            this.groupBoxFilter_DVA.TabStop = false;
+            this.groupBoxFilter_DVA.Text = "Фильтрация";
+            // 
+            // groupBoxSort_DVA
+            // 
+            this.groupBoxSort_DVA.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.groupBoxSort_DVA.Location = new System.Drawing.Point(1058, 168);
+            this.groupBoxSort_DVA.Name = "groupBoxSort_DVA";
+            this.groupBoxSort_DVA.Size = new System.Drawing.Size(271, 105);
+            this.groupBoxSort_DVA.TabIndex = 3;
+            this.groupBoxSort_DVA.TabStop = false;
+            this.groupBoxSort_DVA.Text = "Сортировка";
+            // 
+            // textBoxSearch_DVA
+            // 
+            this.textBoxSearch_DVA.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.textBoxSearch_DVA.Location = new System.Drawing.Point(83, 24);
+            this.textBoxSearch_DVA.Name = "textBoxSearch_DVA";
+            this.textBoxSearch_DVA.Size = new System.Drawing.Size(966, 27);
+            this.textBoxSearch_DVA.TabIndex = 4;
+            // 
+            // labelSearch_DVA
+            // 
+            this.labelSearch_DVA.AutoSize = true;
+            this.labelSearch_DVA.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelSearch_DVA.Location = new System.Drawing.Point(6, 23);
+            this.labelSearch_DVA.Name = "labelSearch_DVA";
+            this.labelSearch_DVA.Size = new System.Drawing.Size(71, 25);
+            this.labelSearch_DVA.TabIndex = 5;
+            this.labelSearch_DVA.Text = "Поиск";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 646);
+            this.Controls.Add(this.panelMain_DVA);
+            this.Controls.Add(this.panelDown_DVA);
+            this.Controls.Add(this.panelRight_DVA);
+            this.Controls.Add(this.panelLeft_DVA);
+            this.Name = "FormMain";
+            this.Text = "Персональные ЭВМ";
+            this.panelMain_DVA.ResumeLayout(false);
+            this.panelMain_DVA.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewViewFile_DVA)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelLeft_DVA;
+        private System.Windows.Forms.Panel panelRight_DVA;
+        private System.Windows.Forms.Panel panelDown_DVA;
+        private System.Windows.Forms.Panel panelMain_DVA;
+        private System.Windows.Forms.DataGridView dataGridViewViewFile_DVA;
+        private System.Windows.Forms.GroupBox groupBoxStatistic_DVA;
+        private System.Windows.Forms.Label labelSearch_DVA;
+        private System.Windows.Forms.TextBox textBoxSearch_DVA;
+        private System.Windows.Forms.GroupBox groupBoxSort_DVA;
+        private System.Windows.Forms.GroupBox groupBoxFilter_DVA;
+    }
+}
+
